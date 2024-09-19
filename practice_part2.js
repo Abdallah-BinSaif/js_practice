3898807966f607dd7cf1e6cad8fc5212da0220bb
Abdallah-BinSaif <binsaif.info@gmail.com>
2024-09-19 19:00:28 +0600

    comment box practice

--- practice_part2.js
+++ practice_part2.js
@@ -179,5 +179,28 @@ function calculator (a, b, operation) {
     }
 }
 
-const addition = calculator(3,5, "divitio")
-console.log(addition)
+const addition = calculator(3,5, "divition")
+// console.log(addition)
+
+
+const phones = [
+    {model: "PhoneA", brand: "Iphone", price: 95000},
+    {model: "PhoneB", brand: "Samsung", price: 40000},
+    {model: "PhoneC", brand: "Oppo", price: 26000},
+    {model: "PhoneD", brand: "Nokia", price: 35000},
+    {model: "PhoneE", brand: "Iphone", price: 105000},
+    {model: "PhoneF", brand: "HTC", price: 48000},
+];
+
+
+function findAveragePhonePrice(phones){
+    if(!Array.isArray(phones)) {
+        return `only array are allowed`;
+    };
+    for (const phone of phones) {
+        console.log(phone)
+    }
+}
+
+const average = findAveragePhonePrice(phones)
+console.log(average)
