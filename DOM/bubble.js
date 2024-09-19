aadba76707a082aee9a93bcf95afc2b91052f0b3
Abdallah-BinSaif <binsaif.info@gmail.com>
2024-09-19 21:09:38 +0600

    dom BUBBLE section done

---
+++ DOM/bubble.js
@@ -0,0 +1,19 @@
+document.getElementById("item-2")
+.addEventListener("click", function(){
+    console.log("second li clicked")
+})
+
+document.getElementById("li-container")
+.addEventListener("click", function(){
+    console.log("li container clicked")
+})
+
+document.getElementById("ul-container")
+.addEventListener("click", function(){
+    console.log("ul container clicked")
+})
+
+document.getElementById("body")
+.addEventListener("click", function(){
+    console.log("html body section clicked")
+})
